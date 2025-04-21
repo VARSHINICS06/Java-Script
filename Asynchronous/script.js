811762d11a1ebc122d99672d27d169e7430f588d
Varshini C S <varshinics123@gmail.com>
2025-04-21 23:38:42 +0530

    Add files via upload

---
+++ Asynchronous/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+// NEW COUNTRIES API URL (use instead of the URL shown in videos):
+// https://restcountries.com/v2/name/portugal
+
+// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
+
+const request=new XMLHttpRequest();
+request.open('GET','https://alexwohlbruck.github.io/cat-facts/');
+request.send();
+console.log(request.responseText);
+
+request.addEventListener('load',function(){
+
+    console.log(this.responseText);
+
+    const data=JSON.parse(this.responseText);
+    console.log(data);
+    
+
+});
